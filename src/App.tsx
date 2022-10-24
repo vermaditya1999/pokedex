c54566475e2c8a9cc66e8d463bba65651a916b4f
Aditya Verma <aditya17009@iiitd.ac.in>
2022-10-24 23:31:27 +0530

    fetch data for pokemon card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import PokemonCard from 'components/PokemonCard';
+
 export default function App() {
 	return (
-		<div>
-			<p>Hello World</p>
+		<div className='w-screen h-screen flex justify-center items-center'>
+			<div>
+				<PokemonCard dataURL='https://pokeapi.co/api/v2/pokemon/1' />
+			</div>
 		</div>
 	);
 }
